96a6eaee3d28f7a3264c109d082aaa4493ea864b
Bruno <brunopr.bpr@gmail.com>
2020-04-28 22:56:48 +0100

    Commented and formated

--- scripts/advancedSearch.js
+++ scripts/advancedSearch.js
@@ -1,91 +1,132 @@
 function advancedForm() {
+  //Unhide the advanced form if the checkbox is checked
   if (document.getElementById('checkBox').checked) {
     document.getElementById('simpleform').style.visibility = 'hidden';
     document.getElementById('advancedform').style.visibility = 'visible';
   } else {
+    //Hide if its not checked
     document.getElementById('simpleform').style.visibility = 'visible';
     document.getElementById('advancedform').style.visibility = 'hidden';
   }
 }
-
+//This function is intended to fetch a book from Google Book API and display to the usar
+//It is called from the submit button from the advanced form
+//It will use the title a/or the author's name to search for a book
+//Only the first 10 are shown
 function getBook() {
 
+  //Get the <div id='results'> that holds the list
   var results = document.getElementById('results');
-
+  //Cleaning the previous result if any
   if (results.childNodes[0] != undefined) {
     results.removeChild(results.childNodes[0])
   }
 
-
+  //Creating a new unordered list <ul>
   const colletion = document.createElement('ul')
+  //Set the classe to the <ul>
   colletion.setAttribute('class', 'collection col s6 offset-s2')
+  //Add to the DIV id
   results.appendChild(colletion)
 
+  //Get the input from the title input field
   const titleInput = document.getElementById('title').value
+  //Get the input from the authot input field
   const authorInput = document.getElementById('author').value
+  //Empty url to be populated
   var url = ""
+  //If the user entered a title and an author add it to the url
   if (titleInput != "" && authorInput != "") {
     url = 'https://www.googleapis.com/books/v1/volumes?q=+intitle:' + titleInput + '+inauthor:' + authorInput + '&key=AIzaSyCfxVfRWs5eLZEwwiZemvp1iAwyAaHBHUQ'
   }
+  //If the user didnt entered an author, just add the title to the url
   if (authorInput == "") {
     url = 'https://www.googleapis.com/books/v1/volumes?q=+intitle:' + titleInput + '&key=AIzaSyCfxVfRWs5eLZEwwiZemvp1iAwyAaHBHUQ'
-  } else if (titleInput == "") {
+  }
+  //If the user didnt entered an title, just add the author to the url
+  else if (titleInput == "") {
     url = 'https://www.googleapis.com/books/v1/volumes?q=+inauthor:' + authorInput + '&key=AIzaSyCfxVfRWs5eLZEwwiZemvp1iAwyAaHBHUQ'
   }
-
-
-
-  console.log(url);
+  //Fetch the Google Books API
   fetch(url)
     .then(response => {
       return response.json()
     })
     .then(data => {
+      //If there is at least one result loop through all of them 
       if (data.totalItems > 0) {
         for (i = 0; i < data.totalItems; i++) {
+          //Those are the components to build materializecss collection component
+          //See more at : https://materializecss.com/collections.html
+          //Create a <li>
           const list = document.createElement('li')
+          //Create a <span>
           const span = document.createElement('span')
+          //Create a <p>
           const p = document.createElement('p')
+          //Create a <img>
           const img = document.createElement('img')
+          //Create a <li>
           const a = document.createElement('a')
+          //Create a <i>
           const button = document.createElement('i')
-
+          //Defining classes to the components for the collections
           list.setAttribute('class', 'collection-item avatar')
           span.setAttribute('class', 'title')
-          if (data.items[i].volumeInfo.imageLinks != undefined) {
-            img.setAttribute('src', data.items[i].volumeInfo.imageLinks.smallThumbnail)
-          }
           img.setAttribute('class', 'circle')
           a.setAttribute('class', 'btn-floating btn-large waves-effect waves-light red')
           button.setAttribute('class', 'material-icons')
+          //If there is a image thumgbail, define it as a logo
+          if (data.items[i].volumeInfo.imageLinks != undefined) {
+            img.setAttribute('src', data.items[i].volumeInfo.imageLinks.smallThumbnail)
+          }
+          //Defining the material-icons for the button
           button.innerText = 'shop';
+          //The info variable stores the information of the book
+          //First, add the title to it
           var info = "Title: " + data.items[i].volumeInfo.title
+          //Store the title to pass it to the Amazon url
           title = data.items[i].volumeInfo.title
+          //If there is a author add it to the info
           if (data.items[i].volumeInfo.authors != undefined) {
             info = info + '\n Author:     ' + data.items[i].volumeInfo.authors[0];
+            //Store the author to pass it to the Amazon url
             author = data.items[i].volumeInfo.authors[0];
           }
+          //If there is a publisher add it to the info
           if (data.items[i].volumeInfo.publisher != undefined) {
             info = info + '\n Publisher:    ' + data.items[i].volumeInfo.publisher;
             publisher = data.items[i].volumeInfo.publisher;
           }
+          //If there is a category add it to the info
           if (data.items[i].volumeInfo.categories != undefined) {
             info = info + '\n Category:    ' + data.items[i].volumeInfo.categories[0];
           }
+          //If there is a published date add it to the info
           if (data.items[i].volumeInfo.publishedDate != undefined) {
             var date = data.items[i].volumeInfo.publishedDate
             info = info + '\n Year:    ' + date.substring(0, 4);
           }
+          // <a> is the red cicle button 
           a.setAttribute('style', 'margin-top: -80px')
+          // This link will send the usar to the amazon site to buy the book
           a.setAttribute('href', 'https://www.amazon.co.uk/s?k=' + title + " " + author + '&ref=nb_sb_noss_2')
+          // To open a new window
           a.setAttribute('target', '_blank')
+          //Add all the info regarding the book to the <p>
           p.innerText = info
+          // Nesting the elements as:
+          // <li> 
+          // ----<img>
+          // ----<p>
+          // ----<a><i>
           list.appendChild(img)
           list.appendChild(p)
           p.appendChild(a)
           colletion.appendChild(list)
           a.appendChild(button)
         }
+        //If no books were found, display an message to the user
       } else {
         const error = document.createElement('p')
         error.setAttribute('style', "color: white; text-align: center")
@@ -94,6 +135,7 @@ function getBook() {
       }
     }
     )
+    //If any error occurs, display a red error message to the user
     .catch(err => {
       console.log(err)
       const error = document.createElement('p')
--- scripts/search.js
+++ scripts/search.js
@@ -1,18 +1,23 @@
-
-
-
+//This function is intended to fetch a book from Google Book API and display to the usar
+//It is called from the submit button from the simple form
+//It will use any word to search for a book
+//Only the first 10 are shown
 function getBooks() {
+  //Get the <div id='results'> that holds the list
   var results = document.getElementById('results');
-
+  //Get the <div id='results'> that holds the list
+  var results = document.getElementById('results');
+  //Cleaning the previous result if any
   if (results.childNodes[0] != undefined) {
     results.removeChild(results.childNodes[0])
   }
-
-
+  //Creating a new unordered list <ul>
   const colletion = document.createElement('ul')
-  colletion.setAttribute('class', 'collection col s6  offset-s2')
+  //Set the classe to the <ul>
+  colletion.setAttribute('class', 'collection col s6 offset-s2')
+  //Add to the DIV id
   results.appendChild(colletion)
-
+  // Get the input
   const book = document.getElementById('search').value
 
   fetch('https://www.googleapis.com/books/v1/volumes?q=' + book + '&key=AIzaSyCfxVfRWs5eLZEwwiZemvp1iAwyAaHBHUQ')
@@ -20,57 +25,89 @@ function getBooks() {
       return response.json()
     })
     .then(data => {
+      //If there is at least one result loop through all of them 
       if (data.totalItems > 0) {
         for (i = 0; i < data.totalItems; i++) {
+          //Those are the components to build materializecss collection component
+          //See more at : https://materializecss.com/collections.html
+          //Create a <li>
           const list = document.createElement('li')
+          //Create a <span>
           const span = document.createElement('span')
+          //Create a <p>
           const p = document.createElement('p')
+          //Create a <img>
           const img = document.createElement('img')
+          //Create a <li>
           const a = document.createElement('a')
+          //Create a <i>
           const button = document.createElement('i')
-
+          //Defining classes to the components for the collections
           list.setAttribute('class', 'collection-item avatar')
           span.setAttribute('class', 'title')
-          img.setAttribute('src', data.items[i].volumeInfo.imageLinks.smallThumbnail)
           img.setAttribute('class', 'circle')
-          a.setAttribute('class', 'btn-floating btn-large waves-effect waves-light red')         
+          a.setAttribute('class', 'btn-floating btn-large waves-effect waves-light red')
           button.setAttribute('class', 'material-icons')
-          button.innerText = 'shop';         
+          //If there is a image thumgbail, define it as a logo
+          if (data.items[i].volumeInfo.imageLinks != undefined) {
+            img.setAttribute('src', data.items[i].volumeInfo.imageLinks.smallThumbnail)
+          }
+          //Defining the material-icons for the button
+          button.innerText = 'shop';
+          //The info variable stores the information of the book
+          //First, add the title to it
           var info = "Title: " + data.items[i].volumeInfo.title
+          //Store the title to pass it to the Amazon url
           title = data.items[i].volumeInfo.title
-          if (data.items[i].volumeInfo.authors != undefined){
+          //If there is a author add it to the info
+          if (data.items[i].volumeInfo.authors != undefined) {
             info = info + '\n Author:     ' + data.items[i].volumeInfo.authors[0];
+            //Store the author to pass it to the Amazon url
             author = data.items[i].volumeInfo.authors[0];
-          } 
-          if(data.items[i].volumeInfo.publisher != undefined){
+          }
+          //If there is a publisher add it to the info
+          if (data.items[i].volumeInfo.publisher != undefined) {
             info = info + '\n Publisher:    ' + data.items[i].volumeInfo.publisher;
             publisher = data.items[i].volumeInfo.publisher;
           }
-          if(data.items[i].volumeInfo.categories != undefined){
+          //If there is a category add it to the info
+          if (data.items[i].volumeInfo.categories != undefined) {
             info = info + '\n Category:    ' + data.items[i].volumeInfo.categories[0];
           }
-          if(data.items[i].volumeInfo.publishedDate != undefined){
+          //If there is a published date add it to the info
+          if (data.items[i].volumeInfo.publishedDate != undefined) {
             var date = data.items[i].volumeInfo.publishedDate
             info = info + '\n Year:    ' + date.substring(0, 4);
           }
-         a.setAttribute('style', 'margin-top: -80px')
-          a.setAttribute('href', 'https://www.amazon.co.uk/s?k='+title+" "+author+'&ref=nb_sb_noss_2')
+          // <a> is the red cicle button 
+          a.setAttribute('style', 'margin-top: -80px')
+          // This link will send the usar to the amazon site to buy the book
+          a.setAttribute('href', 'https://www.amazon.co.uk/s?k=' + title + " " + author + '&ref=nb_sb_noss_2')
+          // To open a new window
           a.setAttribute('target', '_blank')
-          p.innerText = info         
-          list.appendChild(img)      
+          //Add all the info regarding the book to the <p>
+          p.innerText = info
+          // Nesting the elements as:
+          // <li> 
+          // ----<img>
+          // ----<p>
+          // ----<a><i>
+          list.appendChild(img)
           list.appendChild(p)
           p.appendChild(a)
           colletion.appendChild(list)
           a.appendChild(button)
         }
-      } else { 
+        //If no books were found, display an message to the user
+      } else {
         const error = document.createElement('p')
         error.setAttribute('style', "color: white; text-align: center")
         error.innerText = 'Please, try again! \n Your search returned 0 item! :(';
         colletion.appendChild(error)
       }
-      }
+    }
     )
+    //If any error occurs, display a red error message to the user
     .catch(err => {
       console.log(err)
       const error = document.createElement('p')
